a19b05295768392a019735b91302f7c46bb77feb
patriciaSR <patricia.suarez.psr@gmail.com>
2019-11-27 20:41:41 +0100

    add instagram vue application

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+html,
+body,
+#app {
+  margin: 0;
+  font-family: "Roboto", sans-serif;
+  font-size: 14px;
+}
+</style>
